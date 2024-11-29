a0ef3dfa8220aa8d7993dee38fd1fbc98275fdd5
Calebsenm <calebgamercraft@gmail.com>
2024-11-29 18:20:49 -0500

    add method POST in paciente OK

--- Proyectofinal/src/main/java/com/BD1/Proyectofinal/Controller/PacienteController.java
+++ Proyectofinal/src/main/java/com/BD1/Proyectofinal/Controller/PacienteController.java
@@ -20,17 +20,4 @@ public class PacienteController {
     public void postPaciente(@RequestBody Paciente paciente) { 
         pacienteDAO.registrar(paciente); 
     }
-
-    @GetMapping("api/paciente")
-    public List<Paciente> getPacientes() {
-
-        List<Paciente> pacientes = new ArrayList<>();
-        pacientes.add(new Paciente(1, 12345678, "Juan", "Pérez", LocalDate.of(1985, 5, 15), "555-1234",
-                LocalDate.of(2023, 3, 1), "Ciudad A", "Chequeo general", "Dr. Ramírez", 101));
-        pacientes.add(new Paciente(2, 87654321, "María", "Gómez", LocalDate.of(1990, 8, 22), "555-5678",
-                LocalDate.of(2023, 3, 5), "Ciudad B", "Consulta de rutina", "Dr. Rodríguez", 102));
-        pacientes.add(new Paciente(3, 45678912, "Carlos", "Ruiz", LocalDate.of(1975, 11, 30), "555-9876",
-                LocalDate.of(2023, 3, 10), "Ciudad C", "Dolor de cabeza", "Dra. Fernández", 103));
-        return pacientes;
-    }
 }
